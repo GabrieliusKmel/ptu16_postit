eaa4ec4866d5ad125f83bb785d46aace4e725164
GabrieliusKmel <5132zzm@gmail.com>
2023-10-24 13:36:13 +0300

    CRUD API with permissions

---
+++ api_tests/create_post.py
@@ -0,0 +1,24 @@
+import http.client
+import json
+
+connector_class = http.client.HTTPConnection
+
+conn = connector_class('localhost', 8000)
+
+headersList = {
+ "Accept": "*/*",
+ "User-Agent": "PTU16 browser",
+ "Content-Type": "application/json" 
+}
+
+payload = json.dumps({
+  "title": "postinam per python",
+  "body": "dar paprasciau, arba ne...",
+  "user": 1
+})
+
+conn.request("POST", "/posts/", payload, headersList)
+response = conn.getresponse()
+result = json.loads(response.read().decode("utf-8"))
+
+print(result)
---
+++ postit_project/postit_api/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from . import models
+
+class PostSerializer(serializers.ModelSerializer):
+    username = serializers.ReadOnlyField(source='user.username')
+    user_id = serializers.ReadOnlyField(source='user.id')
+
+    class Meta:
+        model = models.Post
+        fields = ['id', 'title', 'body', 'username', 'user_id', 'created_at']
